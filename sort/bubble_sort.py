ae4aa51a431b70f9b20970e0d5b17625e56293db
Junshoong <junshoong@gmail.com>
2017-04-30 00:14:13 +0900

    modify bubble sort

--- sort/bubble_sort.py
+++ sort/bubble_sort.py
@@ -1,5 +1,5 @@
 def bubble_sort(arr):
-    for i in range(len(arr)-1, 1, -1):
+    for i in range(len(arr)-1, 0, -1):
         for j in range(0, i):
             if arr[j] > arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
--- tests/test_sorts.py
+++ tests/test_sorts.py
@@ -3,31 +3,45 @@ import random
 import pytest
 
 
-a = [random.randrange(100) for _ in range(100)]
+a = [random.randrange(10) for _ in range(1000)]
 o = a[:]
 o.sort()
+b = [1, 1, 1, 1, 0]
+o2 = [0, 1, 1, 1, 1]
+c = [5, 5, 4, 4, 3, 3, 2, 2, 1, 1, 0, 0]
+o3 = [0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5]
 
 
-@pytest.mark.skip
+# @pytest.mark.skip
 def test_insertion_sort():
     assert insertion_sort(a) == o
+    assert insertion_sort(b) == o2
+    assert insertion_sort(c) == o3
 
 
-@pytest.mark.skip
+# @pytest.mark.skip
 def test_bubble_sort():
     assert bubble_sort(a) == o
+    assert bubble_sort(b) == o2
+    assert bubble_sort(c) == o3
 
 
-@pytest.mark.skip
+# @pytest.mark.skip
 def test_selection_sort():
     assert selection_sort(a) == o
+    assert selection_sort(b) == o2
+    assert selection_sort(c) == o3
 
 
-@pytest.mark.skip
+# @pytest.mark.skip
 def test_merge_sort():
     assert merge_sort(a) == o
+    assert merge_sort(b) == o2
+    assert merge_sort(c) == o3
 
 
 # @pytest.mark.skip
 def test_quick_sort():
     assert quick_sort(a) == o
+    assert quick_sort(b) == o2
+    assert quick_sort(c) == o3
